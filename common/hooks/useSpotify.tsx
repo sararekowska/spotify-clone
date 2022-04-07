68c2ad3ed5ea2a0c4c711b7280127eb2d86bb260
sararekowska <sara.rekowska@gmail.com>
2022-04-07 13:25:21 +0200

    feat: useSpotify hook

---
+++ common/hooks/useSpotify.tsx
@@ -0,0 +1,18 @@
+import { signIn, useSession } from 'next-auth/react'
+import { useEffect } from 'react'
+import spotifyApi from '../spotify'
+
+export const useSpotify = () => {
+  const { data: session, status } = useSession()
+  useEffect(() => {
+    if (session) {
+      // If refresh access token attempt fails, direct user to login
+      if (session.error === 'RefreshAccessTokenError') {
+        signIn()
+      }
+
+      spotifyApi.setAccessToken(session.user.accessToken!)
+    }
+  }, [session])
+  return spotifyApi
+}
